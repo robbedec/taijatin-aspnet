3093a25d22803e5bc5823e9cd33a13918f5c9962
Robbe Decorte <robbe.decorte@student.hogent.be>
2019-02-21 10:29:14 +0100

    Add project files.

---
+++ G10_ProjectDotNet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace G10_ProjectDotNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
