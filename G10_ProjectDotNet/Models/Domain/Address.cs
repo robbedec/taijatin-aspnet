f0262eaf615dd22d48f0c41fbe885dafa6a020d6
jonasbaert <jonas.baert.w0188@student.hogent.be>
2019-02-22 12:12:09 +0100

    Added some properties and Address class

---
+++ G10_ProjectDotNet/Models/Domain/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace G10_ProjectDotNet.Models.Domain
+{
+    public class Address
+    {
+        public string City { get; set; }
+        public int ZipCode { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+    }
+}
--- G10_ProjectDotNet/Models/Domain/ApplicationUser.cs
+++ G10_ProjectDotNet/Models/Domain/ApplicationUser.cs
@@ -12,6 +12,13 @@ namespace G10_ProjectDotNet.Models
         public int Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
+        public Address Address { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Grade { get; set; }
+        public string Type { get; set; }
+
         public ApplicationUser()
         {
         }
