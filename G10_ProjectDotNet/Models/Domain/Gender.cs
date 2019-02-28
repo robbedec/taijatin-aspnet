dfe2ca97b83af96251c76c62bcde5f61f9d725dc
Jonas Baert <jonas.baert.w0188@student.hogent.be>
2019-02-28 21:20:53 +0100

    Added required fields to personal profile and partly solved a problem

---
+++ G10_ProjectDotNet/Models/Domain/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace G10_ProjectDotNet.Models.Domain
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2,
+        Trans = 3
+    }
+}
