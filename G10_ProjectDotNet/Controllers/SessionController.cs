fe4dae87a7e078348796845f0bcb0e71738b2723
Robbe Decorte <robbe.decorte@student.hogent.be>
2019-03-14 12:25:05 +0100

    niet meer broken

--- G10_ProjectDotNet/Controllers/SessionController.cs
+++ G10_ProjectDotNet/Controllers/SessionController.cs
@@ -16,13 +16,11 @@ namespace G10_ProjectDotNet.Controllers
     {
         private readonly IFormulaRepository _formulaRepository;
         private readonly ISessionRepository _sessionRepository;
-        private readonly IMemberRepository _memberRepository;
 
         public SessionController(IFormulaRepository formulaRepository, ISessionRepository sessionRepository)
         {
             _formulaRepository = formulaRepository;
             _sessionRepository = sessionRepository;
-            _memberRepository = memberRepository;
         }
 
         public IActionResult Index()
@@ -38,7 +36,7 @@ namespace G10_ProjectDotNet.Controllers
         }
 
         [AllowAnonymous]
-        public IActionResult Register(int formulaId)
+        public IActionResult Create(int formulaId)
         {
             int weekday = ((int)DateTime.Now.DayOfWeek == 0) ? 7 : (int)DateTime.Now.DayOfWeek;
             if (_formulaRepository.GetByWeekDay(weekday) == null)
