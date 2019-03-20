518b92c34ac4580e3cca928c9789c41681ee4db7
Jonas Baert <jonas.baert.w0188@student.hogent.be>
2019-03-20 14:06:34 +0100

    Databank seeded with comments

--- G10_ProjectDotNet/Models/Domain/CourseModule.cs
+++ G10_ProjectDotNet/Models/Domain/CourseModule.cs
@@ -12,7 +12,13 @@ namespace G10_ProjectDotNet.Models.Domain
         public Course Course { get; set; }
         public string Url { get; set; }
         public string Text { get; set; }
-        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+        public ICollection<Comment> Comments { get; set; }
+
+        public CourseModule()
+        {
+            Comments = new List<Comment>();
+        }
+
 
         public void AddComment(Comment comment)
         {
